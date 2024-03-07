22bb5202c53454d5db3057e36f1f13deb0f3834f
agisabhi <119267406+agisabhi@users.noreply.github.com>
2024-03-07 10:15:19 +0700

    create migration and db

---
+++ database/migrations/2024_03_07_030214_create_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('transaction_code');
+            $table->dateTime('date_time');
+            $table->bigInteger('total_harga');
+            $table->string('payment_method');
+            $table->string('approval_code')->nullable();
+            $table->string('card_number')->nullable();
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
